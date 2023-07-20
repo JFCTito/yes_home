e0171b1acc0fe65b549e7e00a5e2d736a2c1000b
Victor-N <victornashillustration@gmail.com>
2023-07-20 23:24:58 +0200

    Nav con anclas

--- front-end/src/components/1atoms/componentesVictor/navBarComponents/Logo/Logo.jsx
+++ front-end/src/components/1atoms/componentesVictor/navBarComponents/Logo/Logo.jsx
@@ -4,7 +4,10 @@ import "./Logo.css";
 function Logo (props){
     return(
         /*Hay que meter un figure como padre?*/
-        <img src={props.src} alt="Logo de YesHome" className="logoNav" />
+        <figure className="logoContainer">
+            <a href="" className="ancleLogo"></a>
+            <img src={props.src} alt="Logo de YesHome" className="logoNav" />
+        </figure>
     )
 }
 export default Logo
