968143b31811226f21e510493cc7e635dc756935
tito <jfctito@gmail.com>
2023-07-20 21:35:52 +0200

    routes updated

--- front-end/src/components/5pages/ShowAds.jsx
+++ front-end/src/components/5pages/ShowAds.jsx
@@ -1,88 +1,87 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import CardAdd from "../3organisms/CardAdd.jsx"
-import { ImgAdd } from '../1atoms/ImgAdd/ImgAdd.jsx';
-import { TitleAdd } from '../1atoms/TitleAdd/TitleAdd.jsx';
-import { PriceAdd } from '../1atoms/PriceAdd/PriceAdd.jsx';
-import { LocationAdd } from '../1atoms/LocationAdd/LocationAdd.jsx';
+import { ImgAdd } from "../1atoms/ImgAdd/ImgAdd.jsx";
+import { TitleAdd } from "../1atoms/TitleAdd/TitleAdd.jsx";
+import { PriceAdd } from "../1atoms/PriceAdd/PriceAdd.jsx";
+import { LocationAdd } from "../1atoms/LocationAdd/LocationAdd.jsx";
 
 function ShowAds() {
+  // const [add, setAdd] = useState([]);
+  // const navigate = useNavigate();
 
-    // const [add, setAdd] = useState([]);
-    // const navigate = useNavigate();
+  // const [addList, setAddList] = useState([]);
+  // const [selectedAdd, setSelectedAdd] = useState(null);
 
+  // useEffect(() => {
+  //     fetch('http://localhost:4000/advertisments')
+  //         .then(response => response.json())
+  //         .then(data => setAddList(data.data))
+  //         .catch(error => console.log(error));
+  // }, []);
 
-    // const [addList, setAddList] = useState([]);
-    // const [selectedAdd, setSelectedAdd] = useState(null);
+  // const handleEditAdd = (add) => {
+  //     navigate(`/edit/${add.id}`)
+  // };
 
-    // useEffect(() => {
-    //     fetch('http://localhost:4000/advertisments')
-    //         .then(response => response.json())
-    //         .then(data => setAddList(data.data))
-    //         .catch(error => console.log(error));
-    // }, []);
+  // const handleDeleteAdd = (add) => {
+  //     navigate(`/delete/${add.id}`);
+  // };
 
-    // const handleEditAdd = (add) => {
-    //     navigate(`/edit/${add.id}`)
-    // };
+  // const cards = add.map((add) => <CardAdd
+  //     key={add.id}
+  //     add={add}
+  //     editAdd={handleEditAdd}
+  //     deleteAdd={handleDeleteAdd}
+  // />);
 
-    // const handleDeleteAdd = (add) => {
-    //     navigate(`/delete/${add.id}`);
-    // };
+  // return (
+  //     <div className="addContainer">
+  //         {
+  //             cards
+  //         }
+  //     </div>
+  // )
 
-    // const cards = add.map((add) => <CardAdd
-    //     key={add.id}
-    //     add={add}
-    //     editAdd={handleEditAdd}
-    //     deleteAdd={handleDeleteAdd}
-    // />);
+  const [advertisements, setAdvertisements] = useState([]);
 
-    // return (
-    //     <div className="addContainer">
-    //         {
-    //             cards
-    //         }
-    //     </div>
-    // )
+  useEffect(() => {
+    // Función para realizar la petición a la API
+    const fetchAdvertisements = async () => {
+      try {
+        const response = await fetch(`http://localhost:4000/advertisments`);
+        const data = await response.json();
+        setAdvertisements(data);
+      } catch (error) {
+        console.error("Error al obtener los anuncios:", error);
+      }
+    };
 
-    const [advertisements, setAdvertisements] = useState([]);
+    fetchAdvertisements();
+  }, []);
 
-    useEffect(() => {
-      // Función para realizar la petición a la API
-      const fetchAdvertisements = async () => {
-        try {
-          const response = await fetch("http://localhost:4000/advertisments");
-          const data = await response.json();
-          setAdvertisements(data);
-        } catch (error) {
-          console.error("Error al obtener los anuncios:", error);
-        }
-      };
-  
-      fetchAdvertisements();
-    }, []);
-  
-    return (
-      <div>
-        <h1>Lista de Anuncios</h1>
-        <ul>
-          {advertisements.map((ad) => (
-            <li key={ad.id}>
-              <ImgAdd img={ad.img} style="cardsSmall"></ImgAdd>
-              <TitleAdd title={ad.title} style="titleAdd"></TitleAdd>
-              <PriceAdd detail={ad.price} style="priceAdd"></PriceAdd>
-              <LocationAdd location={ad.localitation} style="locationAdd"></LocationAdd>
-              
-              {/* <p>{ad.mts2}</p>
+  return (
+    <div>
+      <h1>Lista de Anuncios</h1>
+      <ul>
+        {advertisements.map((ad) => (
+          <li key={ad.id}>
+            <ImgAdd img={ad.img} style="cardsSmall"></ImgAdd>
+            <TitleAdd title={ad.title} style="titleAdd"></TitleAdd>
+            <PriceAdd detail={ad.price} style="priceAdd"></PriceAdd>
+            <LocationAdd
+              location={ad.localitation}
+              style="locationAdd"
+            ></LocationAdd>
+            <p>Boton</p>
+            {/* <p>{ad.mts2}</p>
               <p>{ad.category}</p>
               <p>{ad.rooms}</p>
               <p>{ad.type}</p> */}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-  
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default ShowAds;
--- front-end/src/router/index.jsx
+++ front-end/src/router/index.jsx
@@ -6,37 +6,41 @@ import CreateAdd from "../components/5pages/CreateAdd.jsx";
 import EditAdd from "../components/5pages/EditAdd.jsx";
 import DeleteAdd from "../components/5pages/DeleteAdd.jsx";
 import ShowAds from "../components/5pages/ShowAds.jsx";
-
+import DetailAdd from "../pages/DetailAdd";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
         path: "/",
-        element: <Layout />,
-        children:
-        [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/myads",
-                element: <MyAdds />,
-            },
-            {
-                path: "/mysearch",
-                element: <ShowAds/>,
-            },
-            {
-                path: "/create",
-                element: <CreateAdd />,
-            },
-            {
-                path: "/edit",
-                element: <EditAdd />,
-            }, {
-                path: "/delete",
-                element: <DeleteAdd />,
-            },
-        ]
-    }
+        element: <Home />,
+      },
+      {
+        path: "/myads",
+        element: <MyAdds />,
+      },
+      {
+        path: "/mysearch",
+        element: <ShowAds />,
+      },
+      {
+        path: "/detailadd",
+        element: <DetailAdd />,
+      },
+      {
+        path: "/create",
+        element: <CreateAdd />,
+      },
+      {
+        path: "/edit",
+        element: <EditAdd />,
+      },
+      {
+        path: "/delete",
+        element: <DeleteAdd />,
+      },
+    ],
+  },
 ]);
