ec121e37dae12a7e7ae6fa79c7ef42d5b49ea076
ainhoa <ainhoa.aguado1@gmail.com>
2023-07-20 23:32:42 +0200

    footer branch

--- front-end/src/Layout/Layout.jsx
+++ front-end/src/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/4templates/Navbar.jsx";
+import Footer from "../components/4templates/Footer/Footer.jsx";
 
 const Layout = () => {
     return (
         <>
             <Navbar />
             <main><Outlet/></main>
-            <footer>Footer</footer>
+            <footer><Footer/></footer>
+            
         </>
     )
 }
---
+++ front-end/src/components/1atoms/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './logo.css';
+import imgFooter from '../../../img/YesHome.png'
+
+const Logo = () => {
+  return (
+      <div className='logo'>
+           <img src={imgFooter} alt="" />
+      </div>
+  )
+}
+
+export default Logo;
