62563202876f04b0404bc8c15608f722cf4b8013
Francisco Manuel <fsaa93@gmail.com>
2023-07-20 17:57:41 +0200

    api consuming / CORS bye

--- front-end/src/components/5pages/ShowAds.jsx
+++ front-end/src/components/5pages/ShowAds.jsx
@@ -4,41 +4,72 @@ import CardAdd from "../3organisms/CardAdd.jsx"
 
 function ShowAds() {
 
-    const [add, setAdd] = useState([]);
-    const navigate = useNavigate();
+    // const [add, setAdd] = useState([]);
+    // const navigate = useNavigate();
 
 
-    const [addList, setAddList] = useState([]);
-    const [selectedAdd, setSelectedAdd] = useState(null);
+    // const [addList, setAddList] = useState([]);
+    // const [selectedAdd, setSelectedAdd] = useState(null);
+
+    // useEffect(() => {
+    //     fetch('http://localhost:4000/advertisments')
+    //         .then(response => response.json())
+    //         .then(data => setAddList(data.data))
+    //         .catch(error => console.log(error));
+    // }, []);
+
+    // const handleEditAdd = (add) => {
+    //     navigate(`/edit/${add.id}`)
+    // };
+
+    // const handleDeleteAdd = (add) => {
+    //     navigate(`/delete/${add.id}`);
+    // };
+
+    // const cards = add.map((add) => <CardAdd
+    //     key={add.id}
+    //     add={add}
+    //     editAdd={handleEditAdd}
+    //     deleteAdd={handleDeleteAdd}
+    // />);
+
+    // return (
+    //     <div className="addContainer">
+    //         {
+    //             cards
+    //         }
+    //     </div>
+    // )
+
+    const [result, setResult] = useState('');
 
     useEffect(() => {
-        fetch('http://localhost:8080/add')
-            .then(response => response.json())
-            .then(data => setAddList(data.data))
-            .catch(error => console.log(error));
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+  
+      fetch("http://localhost:4000/advertisments", requestOptions)
+        .then(response => response.text())
+        .then(result => setResult(result))
+        .catch(error => console.log('error', error));
     }, []);
-
-    const handleEditAdd = (add) => {
-        navigate(`/edit/${add.id}`)
-    };
-
-    const handleDeleteAdd = (add) => {
-        navigate(`/delete/${add.id}`);
-    };
-
-    const cards = add.map((add) => <CardAdd
-        key={add.id}
-        add={add}
-        editAdd={handleEditAdd}
-        deleteAdd={handleDeleteAdd}
-    />);
-
+  
     return (
-        <div className="addContainer">
-            {
-                cards
-            }
-        </div>
-    )
-}
+      <div>
+        <h1>Advertisements</h1>
+        <p>{result}</p>
+      </div>
+    );
+  }
+  
+  function App() {
+    return (
+      <div>
+        <h1>My React App</h1>
+        <ShowAds />
+      </div>
+    );
+  }
+
 export default ShowAds;
