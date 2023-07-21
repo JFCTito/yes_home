68d84b8c2a4bc4e965cd69e4e0b13b40a5c983ec
Francisco Manuel <fsaa93@gmail.com>
2023-07-21 08:19:25 +0200

    Update ShowAds.jsx
    Sin comentarios

--- front-end/src/components/5pages/ShowAds.jsx
+++ front-end/src/components/5pages/ShowAds.jsx
@@ -91,19 +91,7 @@ function ShowAds() {
             setFilters({ ...filters, localitation: e.target.value })
           }
         />
-        {/* <Link to={props.ruta}>
-          <button className={`btnStyles ${props.style}`} id={props.id}>
-          <p>{props.text}</p>
-          </button>
-        </Link> */}
         <button id="mysearchButton" className="btnCTA" onClick={handleApplyFilters}>Aplicar Filtros</button>
-        {/* <Buttons
-          onClick={handleApplyFilters}
-          style="btnCTA"
-          text="Apply Filters"
-          // ruta="/mysearch"
-          id="mysearchButton"
-        /> */}
       </div>
       <ul>
         {advertisements.map((ad) => (
@@ -123,94 +111,3 @@ function ShowAds() {
 }
 
 export default ShowAds;
-
-// const [advertisements, setAdvertisements] = useState([]);
-// const [filters, setFilters] = useState({
-//   type: "",
-//   category: "",
-//   localitation: "",
-// });
-
-// useEffect(() => {
-//   fetchAdvertisements();
-// }, []);
-
-// const fetchAdvertisements = async () => {
-//   try {
-//     const params = new URLSearchParams();
-    
-//     if (filters.type.trim() !== "") {
-//       params.append("type", filters.type);
-//     }
-//     if (filters.category.trim() !== "") {
-//       params.append("category", filters.category);
-//     }
-//     if (filters.localitation.trim() !== "") {
-//       params.append("localitation", filters.localitation);
-//     }
-
-//     let apiUrl = "http://localhost:4000/advertisments";
-
-//     if (params.toString() !== "") {
-//       apiUrl += "/filter?" + params.toString();
-//     }
-
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     setAdvertisements(data);
-//   } catch (error) {
-//     console.error("Error al obtener los anuncios:", error);
-//   }
-// };
-
-// const handleApplyFilters = () => {
-//   fetchAdvertisements();
-// };
-
-
-// return (
-//   <div>
-//     <h1>Lista de Anuncios</h1>
-//     <div>
-//       <input
-//         type="text"
-//         name="type"
-//         placeholder="Type"
-//         className='input'
-//         value={filters.type}
-//         onChange={(e) => setFilters({ ...filters, type: e.target.value })}
-//       />
-//       <input
-//         type="text"
-//         name="category"
-//         placeholder="Category"
-//         className='input'
-//         value={filters.category}
-//         onChange={(e) => setFilters({ ...filters, category: e.target.value })}
-//       />
-//       <input
-//         type="text"
-//         name="localitation"
-//         placeholder="Localitation"
-//         className='input'
-//         value={filters.localitation}
-//         onChange={(e) => setFilters({ ...filters, localitation: e.target.value })}
-//       />
-//       <button onClick={handleApplyFilters}>Aplicar Filtros</button>
-//     </div>
-//     <ul>
-//       {advertisements.map((ad) => (
-//         <li key={ad.id}>
-//           <ImgAdd img={ad.img} style="cardsSmall"></ImgAdd>
-//           <TitleAdd title={ad.title} style="titleAdd"></TitleAdd>
-//           <PriceAdd detail={ad.price} style="priceAdd"></PriceAdd>
-//           <LocationAdd
-//             location={ad.localitation}
-//             style="locationAdd"
-//           ></LocationAdd>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-// };
